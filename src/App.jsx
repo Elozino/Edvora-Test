7f76d729e25449db67b94c9e46a1432516377b98
Elozino A. Ovedhe <ovedhee@gmail.com>
2022-02-17 10:51:01 +0100

    fixed ui

--- src/App.jsx
+++ src/App.jsx
@@ -74,65 +74,7 @@ function App() {
   return (
     <div className="bg-zinc-800 pb-5">
       <Header user={user} />
-      <main className="px-10 text-gray-100 text-opacity-60">
-        <header className="md:flex justify-between items-center py-5 relative">
-          <div className="flex items-center">
-            <h4 className="mr-4 cursor-pointer px-2 pb-2 border-b-2 border-transparent hover:border-gray-100">
-              Nearest rides
-            </h4>
-            <h4
-              onClick={() => upcomingRides()}
-              className="mr-4 cursor-pointer px-2 pb-2  border-b-2 border-transparent hover:border-gray-100"
-            >
-              Upcoming rides ({upcomingRide.length})
-            </h4>
-            <h4
-              onClick={() => pastRides()}
-              className="mr-4 cursor-pointer px-2 pb-2  border-b-2 border-transparent hover:border-gray-100"
-            >
-              Past rides ({pastRide.length})
-            </h4>
-          </div>
-          <div
-            onClick={() => setShowFilter(!showFilter)}
-            className="border-b-2 border-transparent cursor-pointer flex items-center justify-end "
-          >
-            <img src={filterIcon} alt="image" />
-            <span className="ml-2"> Filter</span>
-            {showFilter && (
-              <>
-                <div className="absolute top-28 md:top-16 right-0 bg-neutral-900 shadow-lg p-3 md:p-5 rounded-lg w-3/5 sm:w-2/5 md:w-1/6">
-                  <h4>Filters</h4>
-                  <hr className="my-3" />
-                  <div
-                    onClick={() => {
-                      setData(Ride);
-                      filterStateCity("state", "Maharashtra2");
-                    }}
-                    className="bg-zinc-800 py-2 px-5 rounded-lg flex items-center justify-between mb-3"
-                  >
-                    <p>State</p>
-                    {/* <p>▽</p> */}
-                    <p>&#9660;</p>
-                  </div>
-                  <div
-                    onClick={() => {
-                      setData(Ride);
-                      filterStateCity("city", "Panvel1");
-                    }}
-                    className="bg-zinc-800 py-2 px-5 rounded-lg flex items-center justify-between mb-3"
-                  >
-                    <p>City</p>
-                    {/* <p>▽</p> */}
-                    <p>&#9660;</p>
-                  </div>
-                </div>
-              </>
-            )}
-          </div>
-        </header>
-      </main>
-      {/* <Filter
+      <Filter
         upcomingRides={upcomingRides}
         upcomingRide={upcomingRide}
         pastRide={pastRide}
@@ -142,7 +84,7 @@ function App() {
         setData={setData}
         Ride={Ride}
         filterStateCity={filterStateCity}
-      /> */}
+      />
 
       <main className="px-10 text-gray-100 text-opacity-60">
         {data?.map((item, i) => {
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import Img from "../../public/assets/profile.jpg";
 
 function Header({ user }) {
   return (
-    <header className="text-gray-100 bg-neutral-900 h-16 flex items-center p-10 justify-between">
+    <header className="text-gray-100 bg-neutral-900 h-16 flex items-center px-5 py-10 md:p-10 justify-between">
       <h2 className="text-4xl font-semibold">Edvora</h2>
       <div className="flex items-center">
         <h4 className="text-xl mr-2">{user.name}</h4>
